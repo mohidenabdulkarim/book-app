1e3077436f376f548b6337670608a037c5679418
Mohiden abdulkarim <mohidenabdi@hotmail.com>
2022-01-20 23:05:17 +0300

    init win

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Tabs from "./navigation/tabs";
+import { BookDetail } from "./screens";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
   },
-});
+};
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  const [loaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={"Home"}>
+        {/* Tabs */}
+        <Stack.Screen name="Home" component={Tabs} />
+
+        {/* Screens */}
+        <Stack.Screen name="BookDetail" component={BookDetail} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ navigation/tabs.tsx
@@ -0,0 +1,77 @@
+import { BottomTabBar, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Image } from "react-native";
+import { COLORS, icons } from "../constants";
+import { Home } from "../screens";
+
+const Tab = createBottomTabNavigator();
+
+// const tabOptions = {
+//   showLabel: false,
+//   style: {
+//     height: "10%",
+//     backgroundColor: COLORS.black,
+//   },
+// };
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          const tintColor = focused ? COLORS.white : COLORS.gray;
+
+          switch (route.name) {
+            case "Home":
+              return (
+                <Image
+                  source={icons.dashboard_icon}
+                  resizeMode="contain"
+                  style={{ tintColor: tintColor, width: 25, height: 25 }}
+                />
+              );
+
+            case "Search":
+              return (
+                <Image
+                  source={icons.search_icon}
+                  resizeMode="contain"
+                  style={{ tintColor: tintColor, width: 25, height: 25 }}
+                />
+              );
+
+            case "Notification":
+              return (
+                <Image
+                  source={icons.notification_icon}
+                  resizeMode="contain"
+                  style={{ tintColor: tintColor, width: 25, height: 25 }}
+                />
+              );
+
+            case "Setting":
+              return (
+                <Image
+                  source={icons.menu_icon}
+                  resizeMode="contain"
+                  style={{ tintColor: tintColor, width: 25, height: 25 }}
+                />
+              );
+          }
+        },
+        //bottom navigation styles goes here
+        // tabBarShowLabel: false,
+        tabBarLabelStyle: { color: "#fff" },
+        headerShown: false,
+        tabBarStyle: { backgroundColor: COLORS.black, height: "10%" },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Home} />
+      <Tab.Screen name="Notification" component={Home} />
+      <Tab.Screen name="Setting" component={Home} />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
